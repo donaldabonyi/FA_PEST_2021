e4002bdab447f5dc0c0cc35d3e5a90b56a7d8ac3
JuliaPelzer <jupelzer@outlook.de>
2021-09-23 21:09:27 +0200

    cleaned up interpolation code

--- pest/bin/interpolation.py
+++ pest/bin/interpolation.py
@@ -4,44 +4,43 @@ import pandas as pd
 import csv
 import h5py
 
+
 def interpolation_main():
     # read pilot points from .dat file
-    pilot_points = input.read_input()
+    pilot_points = read_pilot_points()
 
     # save data from pilot points in extra lists for the interpolation
-    x_coord_pp = pilot_points.loc[:,"x_coord"]
-    y_coord_pp = pilot_points.loc[:,"x_coord"]
-    permeability_pp = pilot_points.loc[:,"permeability"]
+    x_coord_pp = pilot_points.loc[:, "x_coord"]
+    y_coord_pp = pilot_points.loc[:, "x_coord"]
+    permeability_pp = pilot_points.loc[:, "permeability"]
 
     # get grid information
-    x_grid, y_grid = grid.define_grid()
+    x_grid, y_grid = read_grid()
 
     # call the interpolation Rbf
     rbf_function = interpolate.Rbf(x_coord_pp, y_coord_pp, permeability_pp)
-    interpolated_permeability = rbf_function(x_grid[1], y_grid[1])
+    interpolated_permeability = rbf_function(x_grid, y_grid)
     print(type(interpolated_permeability))
+
     # save grid as np array
-    #grid_df = pd.DataFrame([x_grid[1], y_grid[1], interpolated_permeability])
-    cells_grid = np.array([x_grid[1], y_grid[1]])
+    cells_grid = np.array([x_grid, y_grid])
 
-    out.write_permeability_file(interpolated_permeability, cells_grid)
+    # writes the permeability file from the guessed permeability on the grid cells
+    write_permeability_file(interpolated_permeability, cells_grid)
 
-    # testing rbf
-    # print(x_grid[1][0])
-    # print(y_grid[1][0])
-    # print(interpolated_permeability[0])
 
-def read_input():
-    # read pilot points
-    with open('Data/permpp.dat') as dat_file, open('permpp.csv', 'w') as csv_file:
+def read_pilot_points():
+    # read pilot points from .dat file
+    with open('../Data/permpp.dat') as dat_file, open('../Data/permpp.csv', 'w') as csv_file:
         csv_writer = csv.writer(csv_file)
 
         for line in dat_file:
             row = [field.strip() for field in line.split()]
             csv_writer.writerow(row)
 
-    data = pd.read_csv('permpp.csv', names=["ID", "x_coord", "y_coord", "n", "permeability"])
+    data = pd.read_csv('../Data/permpp.csv', names=["ID", "x_coord", "y_coord", "n", "permeability"])
     return data
+
 
 def write_permeability_file(permeability_array, cells_grid):
     # writes the permeability input file for PEST
@@ -51,11 +50,10 @@ def write_permeability_file(permeability_array, cells_grid):
     print(iarray)
 
 
-def define_grid():
+def read_grid():
+    # reads grid file in .h5 format
     filename = '../PFLOTRAN/pflotran_boxmodel.h5'
 
-    list_data_XC = ['XC']
-    list_data_YC = ['YC']
     with h5py.File(filename, "r") as file:
         # List all groups
         a_group_key = list(file.keys())[0]
@@ -64,8 +62,6 @@ def define_grid():
         data = list(file[a_group_key])
         dataset = file['Domain']
 
-        data_xc = [key for key in dataset['XC']]
-        list_data_XC.append(data_xc)
-        data_yc = [key for key in dataset['YC']]
-        list_data_YC.append(data_yc)
+        list_data_XC = [key for key in dataset['XC']]
+        list_data_YC = [key for key in dataset['YC']]
     return list_data_XC, list_data_YC
