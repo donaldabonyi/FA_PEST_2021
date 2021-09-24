88a1b1beb67ac49cd75e4256ec67e3a537c47ccd
JuliaPelzer <jupelzer@outlook.de>
2021-09-24 17:50:53 +0200

    in the middle of debugging interpolation

--- pest/bin/interpolation.py
+++ pest/bin/interpolation.py
@@ -4,21 +4,33 @@ import pandas as pd
 import csv
 import h5py
 
+debug_others = False
+debug = True
+
+def debug_print_others(string_input, *args):
+    # if debug mode then print everything
+    if debug_others:
+        print(string_input % args)
+
+def debug_print(string_input, *args):
+    # if debug mode then print everything
+    if debug:
+        print(string_input % args)
 
 def interpolation_main():
     # read pilot points from .dat file
     pilot_points = read_pilot_points()
-    print('\nPilot_points: \n', pilot_points)
+    debug_print_others('\nPilot_points: \n', pilot_points)
 
     # save data from pilot points in extra lists for the interpolation
     x_coord_pp = pilot_points.loc[:, "x_coord"]
     y_coord_pp = pilot_points.loc[:, "y_coord"]
     permeability_pp = pilot_points.loc[:, "permeability"]
-    print('log permeability_pp: \n', np.log(permeability_pp))
+    debug_print_others('log permeability_pp: \n', np.log(permeability_pp))
 
     # log transform the interpolated values
-    permeability_pp = np.log(permeability_pp)
-    print(x_coord_pp)
+    permeability_pp = np.log10(permeability_pp)
+    debug_print_others(x_coord_pp)
 
     # get grid information
     x_grid, y_grid = read_grid()
@@ -26,25 +38,22 @@ def interpolation_main():
     # call the interpolation Rbf
     rbf_function = interpolate.Rbf(x_coord_pp, y_coord_pp, permeability_pp, function='thin_plate')
     interpolated_permeability = rbf_function(x_grid, y_grid)
-    print('transformed interpolated permeability: \n', interpolated_permeability)
+    debug_print_others('transformed interpolated permeability: \n', interpolated_permeability)
 
     # inv-log transform the interpolated values
-    interpolated_permeability = np.exp(interpolated_permeability)
+    interpolated_permeability = 10**(interpolated_permeability)
     print('interpolated permeability: \n', interpolated_permeability)
 
     # save grid as np array
     cells_grid = np.array([x_grid, y_grid])
-    print('\ncells_grid: \n', cells_grid)
+    debug_print_others('\ncells_grid: \n', cells_grid)
     # writes the permeability file from the guessed permeability on the grid cells
     write_permeability_file(interpolated_permeability, cells_grid)
 
     # Print the created h5 file
     filename = "../PFLOTRAN/permeability_values.h5"
     interpolated_points = read_output_file(filename)
-    print(interpolated_points)
-
-
-
+    debug_print_others(interpolated_points)
 
 def read_pilot_points():
     # read pilot points from .dat file
@@ -63,11 +72,11 @@ def write_permeability_file(permeability_array, cells_grid):
     # writes the permeability input file for PEST
     iarray = np.arange(1, permeability_array.shape[0] + 1, 1)
 
-    if iarray.shape[0] == permeability_array.shape[0] and iarray.shape[0] == cells_grid.shape[1]:
-        print("\n[debug] yes: number of cells matches the number od permeability values")
+    if iarray.shape[0] == cells_grid.shape[1]:
+        debug_print_others("\n[debug] yes: number of cells matches the number od permeability values")
     else:
-        print("\n[debug] no: number of cells doesn't matche the number od permeability values")
-    print(cells_grid.shape[0])
+        debug_print_others("\n[debug] no: number of cells doesn't match the number od permeability values")
+    debug_print_others(cells_grid.shape[0])
     # print('\niarray (cell indices) :', iarray)
 
     file = h5py.File("../PFLOTRAN/permeability_values.h5", "w") #open/create the hdf5 file
@@ -76,22 +85,17 @@ def write_permeability_file(permeability_array, cells_grid):
     for i in range(0,iarray.shape[0]):
         cell_ids[i] = iarray[i]
         perm_vals[i] = permeability_array[i]
-    print('\ncell_ids: \n', cell_ids)
-    print('\nperm_values: \n', perm_vals)
-
-
+    debug_print_others('\ncell_ids: \n', cell_ids)
+    debug_print_others('\nperm_values: \n', perm_vals)
 
 def read_grid():
     # reads grid file in .h5 format
     filename = '../PFLOTRAN/pflotran_boxmodel.h5'
 
     with h5py.File(filename, "r") as file:
-        
-        debug = True
-        if debug:
-            # List all groups
-            a_group_key = list(file.keys())[0]
-            print('\nGroup keys in input h5 file: \n', a_group_key )
+
+        a_group_key = list(file.keys())[0]
+        debug_print_others('\nGroup keys in input h5 file: \n', a_group_key)
 
         dataset = file['Domain']
 
@@ -102,12 +106,9 @@ def read_grid():
 
 def read_output_file(filename):
     with h5py.File(filename, "r") as file:
-        
-        debug = True
-        if debug:
-            # List all groups [debugging]
-            a_group_key = list(file.keys())
-            print('\nGroup keys in created h5 file: \n', a_group_key )
+
+        a_group_key = list(file.keys())
+        debug_print_others('\nGroup keys in created h5 file: \n', a_group_key)
 
         # dataset = file['Permeability']
 
