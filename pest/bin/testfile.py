4c2ce94172d52167abbe9e32766c5698601304ad
JuliaPelzer <jupelzer@outlook.de>
2021-09-24 18:22:07 +0200

    testfile for rbf functionality

--- pest/bin/testfile.py
+++ pest/bin/testfile.py
@@ -1,4 +1,6 @@
-from interpolation import *
+import scipy.interpolate as interpolate
 
-interpolation_main()
+rbf_function = interpolate.Rbf([25, 65], [35, 75], [5,5] , function='thin_plate')
+interpolated_permeability = rbf_function([25, 65], [35, 75])
 
+print('interpolated permeability: \n', interpolated_permeability)
